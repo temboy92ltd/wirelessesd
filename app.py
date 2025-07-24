dc34b28ebf0905d04d7da24310dbfa4d0f43af06
temboy92ltd <61865456+temboy92ltd@users.noreply.github.com>
2025-07-24 09:12:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,38 +1,51 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, request, jsonify
 import random
 
 app = Flask(__name__)
 
-# Danh sách địa chỉ từ C2-081 đến C2-151
-devices = {f'C2-{i:03}': [] for i in range(81, 152)}
+# Danh sách các thiết bị cần mô phỏng
+ #devices = ["C2-131", "C2-132", "C2-133", "C2-134","C2-135","C2-136","C2-137","C2-138","C2-139","C2-140","C2-141","C2-142"]
+
+# Danh sách thiết bị: C2-083 → C2-150
+addresses = [f'C2-{i:03}' for i in range(81, 151)]
+devices = {addr: [] for addr in addresses}
 
 @app.route("/")
 def index():
-    return render_template("Buiding.html")
+    return render_template("Buiding.html",devices=devices.items())
 
-# ESP32 gửi dữ liệu vào đây qua POST
-@app.route("/api/data", methods=["POST"])
-def get_data():
+@app.route("/api/data")
+def receive_data():
+    data = []
     try:
         json_data = request.get_json()
         addr = json_data.get('address')
         data = json_data.get('data')
 
-        if addr in devices and isinstance(data, list):
-            devices[addr] = data
-            print(f"[RECEIVED] {addr}: {data}")
-            return jsonify({"status": "OK"}), 200
+        if addr and isinstance(data, list):
+            if addr in devices:
+                devices[addr] = data
+                print(f"[RECV API] {addr}: {data}")
+                return jsonify({"status": "OK"}), 200
+            else:
+                return jsonify({"error": "Unknown device"}), 400
         else:
-            return jsonify({"error": "Invalid address or data"}), 400
+            return jsonify({"error": "Invalid JSON"}), 400
+
     except Exception as e:
-        return jsonify({"error": str(e)}), 500    
-        
-# Frontend lấy dữ liệu qua GET
-@app.route("/api/data", methods=["GET"])
-def get_data():
-    return jsonify([
-        {"address": addr, "data": devices[addr]} for addr in devices
-    ])
+        print("[ERROR /api/data]", e)
+        return jsonify({"error": str(e)}), 500
+
+
+    # data = []
+    # for device in devices:
+    #     num_led = random.randint(1, 3)  # từ 1 đến 5 LED
+    #     led_data = [random.choice([0, 1]) for _ in range(num_led)]  # 0: đỏ, 1: xanh
+    #     data.append({"address": device, "data": led_data})
+    # return jsonify(data)
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True)
+
+
+#/
